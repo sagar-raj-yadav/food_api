b99d7fcfb93422d631a3c673b3d195f9b6d609be
sinhashan <sagarrajyadav2002@gmail.com>
2024-09-18 17:12:05 +0530

    food_api

---
+++ index.js
@@ -0,0 +1,44 @@
+import express from 'express';
+import { promises as fs } from 'fs';
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Function to read data from a file
+const getDataFromFile = async (fileName) => {
+  try {
+    const data = await fs.readFile(fileName, 'utf8');
+    return JSON.parse(data);
+  } catch (err) {
+    throw new Error(`Error reading ${fileName}: ${err.message}`);
+  }
+};
+
+// Endpoint for data1.json
+app.get('/api/category', async (req, res) => {
+  try {
+    const data = await getDataFromFile('foodCategory.json');
+    res.json(data);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
+// Endpoint for data2.json
+app.get('/api/foodData', async (req, res) => {
+  try {
+    const data = await getDataFromFile('foodData.json');
+    res.json(data);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
+// Root route
+app.get('/', (req, res) => {
+  res.send('Welcome to the ES module JSON API');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
